2f265b2bd2967db52b3b76bb23022d2977706b7f
Jacqueline <jacqueline.cai2004@gmail.com>
2025-03-04 14:21:57 -0500

    pdf only for syllabus uploads

--- backend/src/syllabus/syllabus.route.ts
+++ backend/src/syllabus/syllabus.route.ts
@@ -10,20 +10,10 @@ const upload = multer({
     fileSize: 10 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
-    if (
-      file.mimetype === "application/pdf" ||
-      file.mimetype === "application/msword" ||
-      file.mimetype ===
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
-      file.mimetype === "text/plain"
-    ) {
+    if (file.mimetype === "application/pdf") {
       cb(null, true);
     } else {
-      cb(
-        new Error(
-          "Invalid file type. Only PDF, Word documents, and text files are allowed."
-        )
-      );
+      cb(new Error("Invalid file type. Only PDF files are allowed."));
     }
   },
 });
